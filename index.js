7fd00febb5157f0bcf89509720490ae3dd1aeac7
Marcos Profe <marcos123@email.com>
2021-12-09 15:59:16 -0300

    fix confilct manualy

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ console.log("nueva linea4")
 
 console.log("nueva linea5")
 
-<<<<<<< HEAD
-console.log("nueva linea7")
-=======
-
 console.log("nueva linea6")
->>>>>>> d1cf491a22768a0dd839cd9e3c7b919a00d77a64
+
+console.log("nueva linea7")
+
